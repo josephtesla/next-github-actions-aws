d3ba53c539bd2454c599269ffc416b862664f55b
Joseph <kstdeveloper@gmail.com>
2023-07-24 05:33:13 +0100

    release 2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,8 +17,7 @@ export default function Home() {
       <main className={`${styles.main} ${inter.className}`}>
         <div className={styles.description}>
           <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
+           JOSEPH'S NEXT APP
           </p>
           <div>
             <a
